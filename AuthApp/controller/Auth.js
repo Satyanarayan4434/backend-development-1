77452f65776aed6c0d1e7017e4bfcc5d301b0432
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-01-24 19:57:50 +0530

    signup Functionality added

--- AuthApp/controller/Auth.js
+++ AuthApp/controller/Auth.js
@@ -6,7 +6,7 @@ exports.signup = async(req, res) =>{
         const {name, email, password, role} = req.body;
 
         //find exsisting user
-        const existingUser = User.findOne({email});
+        const existingUser = await User.findOne({email});
         if(existingUser){
             return res.status(400).json({
                 success:false,
