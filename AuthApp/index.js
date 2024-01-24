649dcc423fc3a16cf18019008a1232d26435bbcc
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-01-24 13:27:42 +0530

    setup done

--- AuthApp/index.js
+++ AuthApp/index.js
@@ -6,4 +6,14 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
+//database connection
 require("./config/database").connect();
+
+//route import and mounting
+const user = require("./routes/user");
+app.use("/api/v1", user);
+
+//server activate
+app.listen(PORT , ()=>{
+    console.log(`App is listening at ${PORT}`)
+});
