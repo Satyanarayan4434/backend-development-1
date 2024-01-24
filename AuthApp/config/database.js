b84943feb9aba11bd7c2e312e078c6584a5d72f0
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2024-01-24 18:49:27 +0530

    signup function done

--- AuthApp/config/database.js
+++ AuthApp/config/database.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 exports.connect = () => {
   mongoose
     .connect(process.env.MONGODB_URL)
-    .then(console.log("DB connection succfully"))
+    .then(console.log("DB connection succfull"))
     .catch((err) => {
       console.log("DB connection Issue");
       console.error(err);
---
+++ AuthApp/controller/Auth.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcrypt");
+const User = require("../model/User");
+
+exports.signup = async(req, res) =>{
+    try {
+        const {name, email, password, role} = req.body;
+
+        //find exsisting user
+        const existingUser = User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:"User already exist"
+            })
+        }
+
+        //hasing the password
+        let hashPassword;
+        try {
+           hashPassword = await bcrypt.hash(password, 10); 
+        } catch (error) {
+            return res.status(500).json({
+                success:false,
+                message:"Error in parsing",
+            })
+        }
+
+        //create entry in database
+        const user = await User.create({
+            name, email, password:hashPassword, role
+        })
+        return res.status(200).json({
+            success:true,
+            message:"User created succefully"
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:"Try after some time!!!"
+        })
+    }
+}
